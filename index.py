97a4ac514ab95d42e78f18277f56dff517fffea5
李其军 <>
2019-07-21 12:02:10 +0800

    update

--- index.py
+++ index.py
@@ -1,16 +1,16 @@
 from flask import Flask
 from flask_cors import CORS
-# from routers.router import postMovieClip
+from routers.router import postMovieClip
 UPLOAD_FOLDER = '/uploads'
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 CORS(app)
+# @app.route('/moveClip'
 @app.route('/', methods=['GET'])
 def helle():
     return '<h1>hello</h1>'
-
-
-# @app.route('/moveClip'
-# postMovieClip(app)
+postMovieClip(app)
 # def _moveClip():
 if __name__ == '__main__':
     app.run(debug=False, port=8000, host='172.24.3.233')
+
