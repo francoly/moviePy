f0d5cd292533bff8062e96cdda2545adf5c17be7
李其军 <>
2019-07-21 11:31:15 +0800

    udpate

--- index.py
+++ index.py
@@ -1,12 +1,12 @@
 from flask import Flask
 from flask_cors import CORS
-from routers.router import postMovieClip
+# from routers.router import postMovieClip
 UPLOAD_FOLDER = '/uploads'
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 CORS(app)
 # @app.route('/moveClip'
-postMovieClip(app)
+# postMovieClip(app)
 # def _moveClip():
 if __name__ == '__main__':
     app.run(port=8000)
